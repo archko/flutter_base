d310edfc7dd878c3a3f92c0caa1b5de15d645255
archko <archko@gmail.com>
2020-08-25 12:36:16 +0800

    更新配置到29.0.3,kotlin =1.4.0 添加对话框

---
+++ lib/widget/dialog/dialog_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DialogItem extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String text;
+  final VoidCallback onPressed;
+
+  const DialogItem({Key key, this.icon, this.color, this.text, this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialogOption(
+      onPressed: () {
+        if (onPressed != null) {
+          onPressed.call();
+        } else {
+          Navigator.pop(context);
+        }
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            icon,
+            size: 36,
+            color: color,
+          ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsetsDirectional.only(start: 16),
+              child: Text(text),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
