9ac4df43924ff661e0135667808d64d8beba6634
archko <archko@gmail.com>
2022-10-17 11:35:32 +0800

    取消provider,可以使用getx

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,100 +1,100 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_base/widget/appbar/title_bar.dart';
-import 'package:flutter_base_example/page/home_tabs_page.dart';
-import 'package:flutter_base_example/page/movie_list_page.dart';
-import 'package:flutter_base_example/page/test.dart';
-
-Widget createApp() {
-  return StateDemoApp();
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: StateDemoApp(),
-    );
-  }
-}
-
-class StateDemoApp extends StatefulWidget {
-  const StateDemoApp({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _StateDemoAppState createState() => _StateDemoAppState();
-}
-
-class _StateDemoAppState extends State<StateDemoApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Scaffold(
-        appBar: TitleBar.simpleTitleBar("test"),
-        body: Container(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute<void>(
-                      builder: (_) {
-                        return HomeTabsPage();
-                      },
-                    ),
-                  );
-                },
-                child: Text("HomeTabsPage"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute<void>(
-                      builder: (_) {
-                        return TestApp();
-                      },
-                    ),
-                  );
-                },
-                child: Text("TestApp"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute<void>(
-                      builder: (_) {
-                        return MaterialApp(
-                            theme: ThemeData(
-                              primarySwatch: Colors.green,
-                            ),
-                            home: Scaffold(
-                              appBar: TitleBar.simpleTitleBar("Movie list"),
-                              body: MovieListPage(),
-                            ));
-                      },
-                    ),
-                  );
-                },
-                child: Text("TestList"),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter_base/widget/appbar/title_bar.dart';
+// import 'package:flutter_base_example/page/home_tabs_page.dart';
+// import 'package:flutter_base_example/page/movie_list_page.dart';
+// import 'package:flutter_base_example/page/test.dart';
+//
+// Widget createApp() {
+//   return StateDemoApp();
+// }
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       home: StateDemoApp(),
+//     );
+//   }
+// }
+//
+// class StateDemoApp extends StatefulWidget {
+//   const StateDemoApp({
+//     Key? key,
+//   }) : super(key: key);
+//
+//   @override
+//   _StateDemoAppState createState() => _StateDemoAppState();
+// }
+//
+// class _StateDemoAppState extends State<StateDemoApp> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: Colors.green,
+//       ),
+//       home: Scaffold(
+//         appBar: TitleBar.simpleTitleBar("test"),
+//         body: Container(
+//           margin: EdgeInsets.all(10),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.of(context).push(
+//                     MaterialPageRoute<void>(
+//                       builder: (_) {
+//                         return HomeTabsPage();
+//                       },
+//                     ),
+//                   );
+//                 },
+//                 child: Text("HomeTabsPage"),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.of(context).push(
+//                     MaterialPageRoute<void>(
+//                       builder: (_) {
+//                         return TestApp();
+//                       },
+//                     ),
+//                   );
+//                 },
+//                 child: Text("TestApp"),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.of(context).push(
+//                     MaterialPageRoute<void>(
+//                       builder: (_) {
+//                         return MaterialApp(
+//                             theme: ThemeData(
+//                               primarySwatch: Colors.green,
+//                             ),
+//                             home: Scaffold(
+//                               appBar: TitleBar.simpleTitleBar("Movie list"),
+//                               body: MovieListPage(),
+//                             ));
+//                       },
+//                     ),
+//                   );
+//                 },
+//                 child: Text("TestList"),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- example/lib/page/movie_list_page.dart
+++ example/lib/page/movie_list_page.dart
@@ -1,74 +1,74 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_base/model/provider_widget.dart';
-import 'package:flutter_base_example/model/movie_provider.dart';
-import 'package:flutter_base_example/page/movie_list_item.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
-
-class MovieListPage extends StatefulWidget {
-  MovieListPage({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MovieListPageState();
-  }
-
-  @override
-  String toStringShort() {
-    return "Movie";
-  }
-}
-
-class _MovieListPageState extends State<MovieListPage>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  bool get wantKeepAlive => true;
-  late RefreshController refreshController;
-  late MovieProvider _movieProvider;
-
-  @override
-  void initState() {
-    super.initState();
-    refreshController = RefreshController(initialRefresh: true);
-    _movieProvider = MovieProvider(refreshController: refreshController);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return ProviderWidget<MovieProvider>(
-      model: _movieProvider,
-      onModelInitial: (m) {
-        refreshController.requestRefresh();
-      },
-      builder: (context, model, childWidget) {
-        return Container(
-          margin: EdgeInsets.all(4),
-          child: SmartRefresher(
-            physics: BouncingScrollPhysics(),
-            enablePullDown: true,
-            enablePullUp: true,
-            controller: refreshController,
-            onRefresh: model.loadData,
-            onLoading: model.loadMore,
-            header: MaterialClassicHeader(),
-            footer: ClassicFooter(),
-            child: ListView.builder(
-              itemCount: model.getCount(),
-              itemBuilder: (BuildContext context, int index) =>
-                  _renderItem(context, index),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  _renderItem(context, index) {
-    var item = _movieProvider.data[index];
-    return GestureDetector(
-      onTap: () {},
-      child: MovieListItem(bean: item),
-    );
-  }
-}
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_base/model/provider_widget.dart';
+// import 'package:flutter_base_example/model/movie_provider.dart';
+// import 'package:flutter_base_example/page/movie_list_item.dart';
+// import 'package:pull_to_refresh/pull_to_refresh.dart';
+//
+// class MovieListPage extends StatefulWidget {
+//   MovieListPage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _MovieListPageState();
+//   }
+//
+//   @override
+//   String toStringShort() {
+//     return "Movie";
+//   }
+// }
+//
+// class _MovieListPageState extends State<MovieListPage>
+//     with AutomaticKeepAliveClientMixin {
+//   @override
+//   bool get wantKeepAlive => true;
+//   late RefreshController refreshController;
+//   late MovieProvider _movieProvider;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     refreshController = RefreshController(initialRefresh: true);
+//     _movieProvider = MovieProvider(refreshController: refreshController);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     super.build(context);
+//     return ProviderWidget<MovieProvider>(
+//       model: _movieProvider,
+//       onModelInitial: (m) {
+//         refreshController.requestRefresh();
+//       },
+//       builder: (context, model, childWidget) {
+//         return Container(
+//           margin: EdgeInsets.all(4),
+//           child: SmartRefresher(
+//             physics: BouncingScrollPhysics(),
+//             enablePullDown: true,
+//             enablePullUp: true,
+//             controller: refreshController,
+//             onRefresh: model.loadData,
+//             onLoading: model.loadMore,
+//             header: MaterialClassicHeader(),
+//             footer: ClassicFooter(),
+//             child: ListView.builder(
+//               itemCount: model.getCount(),
+//               itemBuilder: (BuildContext context, int index) =>
+//                   _renderItem(context, index),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+//
+//   _renderItem(context, index) {
+//     var item = _movieProvider.data[index];
+//     return GestureDetector(
+//       onTap: () {},
+//       child: MovieListItem(bean: item),
+//     );
+//   }
+// }
