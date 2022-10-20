18beb09a54333ae7b9d068e5f29d73b81addea4f
archko <archko@gmail.com>
2022-10-20 10:45:27 +0800

    更新浏览器参数

--- lib/widget/browser.dart
+++ lib/widget/browser.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base/utils/string_utils.dart';
 import 'package:webview_flutter_plus/webview_flutter_plus.dart';
 
 class Browser extends StatelessWidget {
-  static open(BuildContext context, String url,
-      {required String title, String? waitingTxt}) {
+  static open(
+      BuildContext context, String url, String? title, String? waitingTxt) {
     Navigator.of(context).push(
       new MaterialPageRoute<void>(
         builder: (_) {
@@ -18,23 +19,18 @@ class Browser extends StatelessWidget {
   }
 
   final String url;
-  final String title;
+  final String? title;
   final String? waitingTxt;
 
-  const Browser(
-      {Key? key, required this.url, required this.title, this.waitingTxt})
+  const Browser({Key? key, required this.url, this.title, this.waitingTxt})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(StringUtils.isEmpty(title) ? "" : title!),
       ),
-      //body: WebView(
-      //  initialUrl: url,
-      //  javascriptMode: JavascriptMode.unrestricted,
-      //),
       body: WebViewPlus(
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (controller) {
@@ -46,18 +42,6 @@ class Browser extends StatelessWidget {
           controller.loadUrl(url);
         },
       ),
-      /*WebviewScaffold(
-        url: url,
-        withZoom: true,
-        withLocalStorage: true,
-        hidden: true,
-        initialChild: Container(
-          color: Colors.white10,
-          child: Center(
-            child: Text(waitingTxt != null ? waitingTxt! : 'Waiting.....'),
-          ),
-        ),
-      ),*/
     );
   }
 }
